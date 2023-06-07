f1745cbd580d40c9a4d5a4ea3862a6a9477459c7
hosseinhj1380 <hosseinhj1380@gmail.com>
2023-06-07 13:07:53 +0330

    delete evrything

--- article_module/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArticleModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'article_module'
-
-
-verbose_name = "دسته بندی مقالات"
--- home_module/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Home_page
-app_name = "home"
-urlpatterns = [
-    path('', Home_page.as_view() ,name='home_page')
-]
